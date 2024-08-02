323cf066f1f0f25cdfd212e289adc6f9bd576b32
Casey Watts <casey.s.watts@gmail.com>
2024-08-02 19:58:43 -0400

    pressure shows up now!

---
+++ src/components/FormattedTime.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let time;
+  const dateObject = new Date(time * 1000); // unix time is seconds, not milliseconds
+</script>
+
+<time class="inline" datetime={dateObject.toISOString()}>
+  {dateObject.toLocaleString("en-us")}
+</time>
---
+++ src/components/WeatherWidget.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let weatherData = {};
+    import FormattedTime from "./FormattedTime.svelte";
+  import Formattedtime from "./FormattedTime.svelte";
+</script>
+
+<div class="w-screen h-screen" style="height: 100vh;">
+  <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+    {#each weatherData.hourly.data as d, i}
+      <!-- <FormattedTime time={d.time} /> -->
+      <rect width="0.8rem" height="{(d.pressure - 1000)}rem" fill="blue" x="{i*1}rem"/>
+      {/each}
+  </svg>
+</div>
