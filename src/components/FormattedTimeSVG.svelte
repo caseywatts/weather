e0e0b39d4f2c72c498b9f20d993e04c0c797a352
Casey Watts <casey.s.watts@gmail.com>
2024-08-02 20:05:20 -0400

    svg

---
+++ src/components/FormattedTimeSVG.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let time;
+  const dateObject = new Date(time * 1000); // unix time is seconds, not milliseconds
+</script>
+
+<text transform="rotate(90)">
+  {dateObject.toLocaleString("en-us")}
+</text>
--- src/components/WeatherWidget.svelte
+++ src/components/WeatherWidget.svelte
@@ -1,14 +1,15 @@
 <script>
   export let weatherData = {};
-    import FormattedTime from "./FormattedTime.svelte";
-  import Formattedtime from "./FormattedTime.svelte";
+    import FormattedTimeSVG from "./FormattedTimeSVG.svelte";
 </script>
 
 <div class="w-screen h-screen" style="height: 100vh;">
   <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
     {#each weatherData.hourly.data as d, i}
-      <!-- <FormattedTime time={d.time} /> -->
-      <rect width="0.8rem" height="{(d.pressure - 1000)}rem" fill="blue" x="{i*1}rem"/>
+      <g transform="translate({i*15}, 0)">
+        <rect width="0.8rem" height="{(d.pressure - 1000)}rem" fill="blue"/>
+        <FormattedTimeSVG time={d.time}/>
+      </g>
       {/each}
   </svg>
 </div>
